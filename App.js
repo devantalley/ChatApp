138f3880caafa7dd0c27cbf04ac368c13b7a108f
devantalley <46549326+devantalley@users.noreply.github.com>
2019-05-28 22:11:18 -0700

    added chatapp files

---
+++ App.js
@@ -0,0 +1,46 @@
+import React from 'react'
+// import Chatkit from '@pusher/chatkit'
+import MessageList from './components/MessageList'
+import SendMessageForm from './components/SendMessageForm'
+import RoomList from './components/RoomList'
+import NewRoomForm from './components/NewRoomForm'
+
+import { tokenUrl, instanceLocator } from './config'
+
+class App extends React.Component {
+    
+    componentDidMount() {
+        const chatManager = new Chatkit.ChatManager({
+            instanceLocator,
+            userId: 'jenny', /** swap out */
+            tokenProvider: new Chatkit.TokenProvider({
+                url: tokenUrl
+            })
+        })
+        
+        chatManager.connect()
+        .then(currentUser => {
+            currentUser.subscribeToRoom({
+                roomId: 21260395, /** swap out */
+                hooks: {
+                    onNewMessage: message => {
+                        console.log('message.text: ', message.text);
+                    }
+                }
+            })
+        })
+    }
+    
+    render() {
+        return (
+            <div className="app">
+                <RoomList />
+                <MessageList />
+                <SendMessageForm />
+                <NewRoomForm />
+            </div>
+        );
+    }
+}
+
+export default App
---
+++ config.js
@@ -0,0 +1,7 @@
+/** swap out */
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/b494ef21-1507-4407-b49b-aaaca6243bf7/token";
+
+/** swap out */
+const instanceLocator = "v1:us1:b494ef21-1507-4407-b49b-aaaca6243bf7";
+
+export { tokenUrl, instanceLocator }
